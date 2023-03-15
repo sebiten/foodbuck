154f24257f62286aa4e29300154aadae1b83ce97
Sebiten <sebdevspace@gmail.com>
2023-03-15 12:50:53 -0300

    first seccion

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,97 +1,44 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
     <>
       <div className="relative">
-        <div className="absolute inset-0">
-          <div className="carousel relative">
-            <div className="carousel-inner relative overflow-hidden w-full">
-              <input
-                className="carousel-open"
-                type="radio"
-                id="carousel-1"
-                name="carousel"
-                aria-hidden="true"
-                hidden
-                checked
-              />
-              <div
-                className="carousel-item absolute opacity-0"
-                style={{ height: "50vh" }}
-              >
-                <div className="block h-full w-full bg-red"></div>
-              </div>
-              <input
-                className="carousel-open"
-                type="radio"
-                id="carousel-2"
-                name="carousel"
-                aria-hidden="true"
-                hidden
-              />
-              <div
-                className="carousel-item absolute opacity-0"
-                style={{ height: "50vh" }}
-              >
-                <div className="block h-full w-full bg-oliveGreen"></div>
-              </div>
-              <input
-                className="carousel-open"
-                type="radio"
-                id="carousel-3"
-                name="carousel"
-                aria-hidden="true"
-                hidden
-              />
-              <div
-                className="carousel-item absolute opacity-0"
-                style={{ height: "50vh" }}
-              >
-                <div className="block h-full w-full bg-gold"></div>
-              </div>
-              <label
-                htmlFor="carousel-3"
-                className="carousel-control absolute bottom-0 right-0 w-12 h-12 bg-white text-black rounded-full cursor-pointer hidden"
-              ></label>
-              <label
-                htmlFor="carousel-2"
-                className="carousel-control absolute bottom-0 right-0 w-12 h-12 bg-white text-black rounded-full cursor-pointer hidden"
-              ></label>
-              <label
-                htmlFor="carousel-1"
-                className="carousel-control absolute bottom-0 right-0 w-12 h-12 bg-white text-black rounded-full cursor-pointer"
-              ></label>
-            </div>
-          </div>
+        <div className="absolute inset-0"></div>
+        <div className="absolute inset-0 bg-gray-900 opacity-40 h-screen">
+          <Image
+            className="h-full w-full object-cover"
+            width={1920}
+            height={1080}
+            src="/hd.jpg"
+            alt="hero"
+          />
         </div>
-        <div className="relative pt-32 pb-10">
+        <div className="relative pb-10 flex flex-col items-center justify-center min-h-screen">
           <div className="container mx-auto px-4">
-            <div className="flex flex-wrap justify-center">
+            <div className="flex flex-wrap justify-center items-center">
               <div className="w-full lg:w-6/12 px-4">
                 <div className="text-center">
-                  <h1 className="text-5xl font-semibold text-brownDark">
-                    Bienvenidos a nuestro restaurante
+                  <h1 className="text-5xl font-semibold text-white mb-4">
+                    Bienvenido a <span className="text-red">Foodbud</span>
                   </h1>
-                  <p className="text-lg leading-relaxed mt-4 mb-4 text-grayLight">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    dictum leo nulla, a porttitor nisi fringilla vel. Quisque
-                    pharetra eleifend nibh, ac malesuada ex posuere ac. Nam eu
-                    velit et arcu tempor auctor. Ut luctus, sapien eget
-                    consequat posuere, mi quam ullamcorper erat, ut rhoncus
-                    mauris lacus ut enim.
+                  <p className="text-lg text-gray-300 mb-10">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, quod. Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Quisquam, quod.
                   </p>
-                  <a
+                  <Link
                     href="#"
                     className="bg-red text-white font-bold rounded-full px-8 py-3 mr-4 hover:bg-gray-900"
                   >
                     Reserva ahora
-                  </a>
+                  </Link>
                   <a
                     href="#"
-                    className="bg-transparent text-brownDark font-bold rounded-full px-8 py-3 hover:bg-gray-900 border-2 border-brownDark"
+                    className="bg-white text-gray-800 font-bold rounded-full px-8 py-3 hover:bg-gray-900"
                   >
-                    Menú
+                    Conoce más
                   </a>
                 </div>
               </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,31 @@ import React from "react";
 function NavBar({ currentTheme, setTheme }) {
   return (
     <nav
-      className={`flex justify-between items-center py-4 px-8 bg-${currentTheme}-100`}
+      className={`flex justify-between items-center py-4 px-8 fixed w-full z-10 bg-white shadow-md`}
     >
-      <div>
-        <Image src="/logo.svg" alt="logo" width={50} height={50} />
+      <div className="flex gap-2">
+        <Image src="/icono_pizza.svg" alt="logo" width={55} height={55} />
+        <span className="text-2xl font-bold text-gray-800">Foodbud</span>
       </div>
-      <div>
+      <div className="flex gap-10">
+        <ul className="flex justify-between gap-10 xl:mr-10">
+          <li>
+            <a
+              href="#"
+              className={`text-gray-800 hover:text-gray-600 font-semibold text-lg`}
+            >
+              Inicio
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className={`text-gray-800 hover:text-gray-600 font-semibold text-lg`}
+            >
+              Menú
+            </a>
+          </li>
+        </ul>
         {currentTheme === "dark" ? (
           <button
             className="bg-black-700 hover:bg-black text-white rounded-md border-pink-600 border-2 px-4 py-2"
@@ -19,7 +38,7 @@ function NavBar({ currentTheme, setTheme }) {
           </button>
         ) : (
           <button
-            className="bg-white hover:bg-gray-100 rounded-md border-pink-600 border-2 px-4 py-2"
+            className="bg-gray-400 hover:bg-gray-500 rounded-md border-pink-600 border-2 px-4 py-2"
             onClick={() => setTheme("dark")}
           >
             <Image src="/moon.svg" alt="dark mode" width={20} height={20} />
